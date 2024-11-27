a63d8fadf6d0bb850aa95d446b703932a314423e
Paco Muro <paco@studioborabora.com>
2024-11-27 23:26:54 +0100

    wip comment our carousel

--- components/Recommendations.vue
+++ components/Recommendations.vue
@@ -2,11 +2,11 @@
   <DefaultGrid>
     <div class="lg:col-start-3 lg:col-end-11 mt-5 lg:mt-10">
       <h4 class="text-2xl font-semibold mb-6">{{ headline }}</h4>
-      <Carousel :items="filteredGroupedItems.slice(0, 3)">
+      <!-- <Carousel :items="filteredGroupedItems.slice(0, 3)">
         <template #slide="{ item }">
           <ItemsHighlightConcert :item="item" />
         </template>
-      </Carousel>
+      </Carousel> -->
     </div>
   </DefaultGrid>
 </template>
