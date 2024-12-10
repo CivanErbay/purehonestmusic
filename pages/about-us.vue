627646d6c77ed225bc6ac4c5d0048cf0485fbce7
Paco Muro <paco@studioborabora.com>
2024-12-10 11:17:25 +0100

    about us page

---
+++ pages/about-us.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <DefaultGrid :no-spacing="true" class="mt-5 lg:mt-10 mb-8">
+      <div
+        class="lg:col-start-3 p-4 md:p-8 lg:col-end-11 flex bg-[#242424] rounded-lg"
+      >
+        <UtilsRichTextRenderer :nodes="settingsData.aboutUs" />
+      </div>
+    </DefaultGrid>
+  </div>
+</template>
+
+<script setup>
+const { data: settingsData } = await fetchGlobalHandler('settings');
+</script>
+
+<style lang="scss" scoped></style>
