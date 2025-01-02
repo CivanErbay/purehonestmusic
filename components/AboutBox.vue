ad80f253b0d5c48cdc505255ec98520baa043429
Markus Gotzmann <markusgotzmann@MBP-von-Markus.fritz.box>
2025-01-02 20:13:24 +0100

    add seo text changes and styling

--- components/AboutBox.vue
+++ components/AboutBox.vue
@@ -2,19 +2,13 @@
   <DefaultGrid>
     <div class="xl:col-start-3 xl:col-end-11">
       <div class="bg-[#242424] w-full p-8 rounded-xl">
-        <h2 class="uppercase text-primary text-base">Willkommen bei PURE.HONEST.MUSICE
+        <h2 class="uppercase text-primary text-base">Willkommen bei LOUD & LOCALE
 </h2>
         <h3 class="uppercase text-text text-[22px] font-semibold mb-5">
           Entdecke noch heute Kölns Konzert-Highlights und sei live mit dabei!
         </h3>
-        <p class="opacity-40 dynamicLineHeight2">
-          Pure.Honest.Music ist nicht nur eine Plattform für alternative Musikliebhaber*innen, sondern auch eine innovative Konzert-Suchmaschine für den Raum Köln. Wir bieten allen, die auf der Suche nach spannenden und außergewöhnlichen Musikveranstaltungen sind, eine einfache Möglichkeit, Konzerte und Events zu entdecken. Unsere Mission ist es, die lebendige Musikszene Kölns sichtbarer zu machen und Menschen mit einer Leidenschaft für alternative Klänge zusammenzubringen.
-
-          Auf unserer Plattform findest du eine handverlesene Auswahl an Konzerten und Events aus Genres wie Alternative, Indie, Post-Punk, Experimental und vielem mehr. Von etablierten Künstlerinnen bis hin zu Newcomern – wir präsentieren dir die spannendsten musikalischen Highlights, die Köln und Umgebung zu bieten haben. Unser Fokus liegt dabei auf der engen Zusammenarbeit mit lokalen Veranstalterinnen und Musikschaffenden, um die Vielfalt der Kölner Musikszene zu fördern.
-
-          Besonders am Herzen liegt uns die Unterstützung von Kölner Nachwuchskünstlerinnen. Mit Pure.Honest.Music bieten wir ihnen eine Bühne und schaffen Gelegenheiten, ihre Musik einem breiteren Publikum zu präsentieren. Gleichzeitig laden wir dich ein, neue Klänge und spannende Talente zu entdecken, die vielleicht schon bald die großen Bühnen erobern.
-
-          Unsere Plattform inspiriert dich dazu, deinen musikalischen Horizont zu erweitern, neue Lieblingsgenres zu erkunden und die Vielfalt der Musikszene in Köln zu feiern. Egal, ob du nach einem intimen Indie-Konzert suchst oder die neuesten Experimente im Post-Punk erleben möchtest – bei Pure.Honest.Music wirst du fündig. Tauche ein in die Welt der Musik und entdecke das Beste, was die Kölner Musikszene zu bieten hat.
+        <p class="opacity-40 dynamicLineHeight2 textJustify">
+          LOUD & LOCAL ist nicht nur eine Plattform für alternative Musikliebhaber*innen, sondern auch eine innovative Konzert-Suchmaschine für den Raum Köln. Wir bieten allen, die auf der Suche nach spannenden und außergewöhnlichen Musikveranstaltungen sind, eine einfache Möglichkeit, Konzerte und Events zu entdecken. Unsere Mission ist es, die lebendige Musikszene Kölns sichtbarer zu machen und Menschen mit einer Leidenschaft für alternative Klänge zusammenzubringen. Auf unserer Plattform findest du eine handverlesene Auswahl an Konzerten und Events aus Genres wie Alternative, Indie, Post-Punk, Experimental und vielem mehr. Von etablierten Künstlerinnen bis hin zu Newcomern – wir präsentieren dir die spannendsten musikalischen Highlights, die Köln und Umgebung zu bieten haben. Unser Fokus liegt dabei auf der engen Zusammenarbeit mit lokalen Veranstalterinnen und Musikschaffenden, um die Vielfalt der Kölner Musikszene zu fördern. Besonders am Herzen liegt uns die Unterstützung von Kölner Nachwuchskünstlerinnen. Mit LOUD & LOCAL bieten wir ihnen eine Bühne und schaffen Gelegenheiten, ihre Musik einem breiteren Publikum zu präsentieren. Gleichzeitig laden wir dich ein, neue Klänge und spannende Talente zu entdecken, die vielleicht schon bald die großen Bühnen erobern. Unsere Plattform inspiriert dich dazu, deinen musikalischen Horizont zu erweitern, neue Lieblingsgenres zu erkunden und die Vielfalt der Musikszene in Köln zu feiern. Egal, ob du nach einem intimen Indie-Konzert suchst oder die neuesten Experimente im Post-Punk erleben möchtest – bei LOUD & LOCAL wirst du fündig. Tauche ein in die Welt der Musik und entdecke das Beste, was die Kölner Musikszene zu bieten hat.
         </p>
       </div>
     </div>
