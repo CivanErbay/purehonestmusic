4d203621041b6a40b1167eb7a673486e521fbd53
Markus Gotzmann <markusgotzmann@MBP-von-Markus.fritz.box>
2025-01-05 22:07:13 +0100

    add blur-sm again

--- components/atom/ArtistCard.vue
+++ components/atom/ArtistCard.vue
@@ -2,7 +2,7 @@
   <div class="">
     <div class="rounded-xl overflow-hidden h-full flex flex-col">
       <div class="h-24 w-full relative">
-        <AtomMedia class="w-full h-full custom-blur-sm blur-sm relative" :isCover="true" v-bind="artist.heroImage" />
+        <AtomMedia class="w-full h-full blur-sm relative" :isCover="true" v-bind="artist.heroImage" />
         <div
           class="w-24 z-20 h-24 overflow-hidden rounded-full top-full absolute left-1/2 -translate-x-1/2 -translate-y-1/2">
           <AtomMedia class="w-full h-full" v-bind="artist.heroImage" :isCover="true" />
@@ -29,9 +29,5 @@
 const props = defineProps({ artist: Object });
 </script>
 
-<style lang="scss" scoped>
-.custom-blur-sm {
-  @apply blur-sm;
-  --tw-drop-shadow: none;
-}
-</style>
+<style lang="scss" scoped></style>
+
