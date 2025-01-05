3009c11210995cbf9d16b6744e211f894c0fab06
Markus Gotzmann <markusgotzmann@MBP-von-Markus.fritz.box>
2025-01-05 18:17:57 +0100

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   app: {
     head: {
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon_16_16.png', sizes: '16x16' },
+        { rel: 'icon', type: 'image/png', href: '/favicon_32_32.png', sizes: '32x32' },
+      ],
       script: [{ src: '/preline.js', type: 'module', defer: true }],
     },
   },
