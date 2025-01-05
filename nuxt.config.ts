3cc00c2f716e1573542606ff09d0c62a172393ba
Markus Gotzmann <markusgotzmann@MBP-von-Markus.fritz.box>
2025-01-05 20:56:34 +0100

    add icon pngs for ios and androit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     head: {
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon_16_16.png', sizes: '16x16' },
         { rel: 'icon', type: 'image/png', href: '/favicon_32_32.png', sizes: '32x32' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' }
+      ],
+      meta: [
+        { name: 'apple-mobile-web-app-capable', content: 'yes' },
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
+        { name: 'theme-color', content: '#ffffff' }
       ],
       script: [{ src: '/preline.js', type: 'module', defer: true }],
     },
