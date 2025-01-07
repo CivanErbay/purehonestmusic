d733e2e453a55d947d088f6eabe8c1242e14fd99
Paco Muro <paco@studioborabora.com>
2025-01-07 13:52:57 +0100

    no suspense

--- app.vue
+++ app.vue
@@ -2,9 +2,7 @@
   <div class="mx-4 lg:mx-0 font-montserrat relative">
     <div class="max-w-[2056px] mx-auto lg:px-16">
       <Navbar />
-      <Suspense>
-        <NuxtPage />
-      </Suspense>
+      <NuxtPage />
       <Footer :settings="settings" />
       <transition name="fade">
         <CookieBanner
