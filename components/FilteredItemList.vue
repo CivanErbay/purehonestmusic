26c8aa64b7a241ae7180abc32e64036882387ebb
Paco Muro <paco@studioborabora.com>
2025-01-17 17:36:35 +0100

    no date filter if less than 7 concerts

--- components/FilteredItemList.vue
+++ components/FilteredItemList.vue
@@ -26,46 +26,37 @@ const filteredItems = computed(() => {
 
   const selectedDate = route.query.date;
 
-  return (
-    concertStore.concerts
-      // .filter(
-      //   (item) =>
-      //     item.name.toLowerCase().includes(query) ||
-      //     item.venue?.address.city.toLowerCase().includes(query) ||
-      //     item.venue?.name.toLowerCase().includes(query) ||
-      //     item.promoter?.name.toLowerCase().includes(query) ||
-      //     item.genres.some((genre) => genre.name?.toLowerCase().includes(query))
-      // )
-      .filter((item) => {
-        const itemDate = new Date(item.date);
-        if (selectedDate) {
-          const newItemDate = new Date(itemDate).toISOString().split('T')[0];
-          return newItemDate === selectedDate;
-        }
-        const maxDate = new Date(today);
-        maxDate.setDate(maxDate.getDate() + concertStore.showUntilDaysFromNow);
-        maxDate.setHours(0, 0, 0, 0);
-        if (itemDate > maxDate) {
-          return false;
-        }
-        return itemDate >= today;
-      })
-      .filter((item) => {
-        const venueMatch =
-          selectedVenues.length === 0 ||
-          selectedVenues.includes(item.venue?.slug);
-        const promoterMatch =
-          selectedPromoters.length === 0 ||
-          selectedPromoters.includes(item.promoter?.slug);
-        const genreMatch =
-          selectedGenres.length === 0 ||
-          selectedGenres.some((genre) =>
-            item.genres.map((g) => g.name).includes(genre)
-          );
-        return venueMatch && promoterMatch && genreMatch;
-      })
-      .sort((a, b) => new Date(a.date) - new Date(b.date))
-  );
+  const beforeDateFilteredConcerts = concertStore.concerts.filter((item) => {
+    const venueMatch =
+      selectedVenues.length === 0 || selectedVenues.includes(item.venue?.slug);
+    const promoterMatch =
+      selectedPromoters.length === 0 ||
+      selectedPromoters.includes(item.promoter?.slug);
+    const genreMatch =
+      selectedGenres.length === 0 ||
+      selectedGenres.some((genre) =>
+        item.genres.map((g) => g.name).includes(genre)
+      );
+    return venueMatch && promoterMatch && genreMatch;
+  });
+
+  // we won't enforce a date filter if there are no more than 7 concerts
+  return beforeDateFilteredConcerts.filter((item) => {
+    const itemDate = new Date(item.date);
+    if (selectedDate) {
+      const newItemDate = new Date(itemDate).toISOString().split('T')[0];
+      return newItemDate === selectedDate;
+    }
+    const maxDate = new Date(today);
+    maxDate.setDate(maxDate.getDate() + concertStore.showUntilDaysFromNow);
+    maxDate.setHours(0, 0, 0, 0);
+
+    if (beforeDateFilteredConcerts.length > 7 && itemDate > maxDate) {
+      return false;
+    }
+    return itemDate >= today;
+  });
+  // .sort((a, b) => new Date(a.date) - new Date(b.date));
 });
 </script>
 
