d7ccbb2bd40b6c6a53474c702c65ca13cde8a165
Markus Gotzmann <markusgotzmann@bbb32933-106e-4caa-ae56-811fdfa81485.fritz.box>
2025-05-26 12:34:03 +0200

    add changes to seo-text

--- components/AboutBox.vue
+++ components/AboutBox.vue
@@ -6,13 +6,19 @@
         <h3 class="uppercase text-text text-[22px] font-semibold mb-5">
           Entdecke noch heute Kölns Konzert-Highlights und sei live mit dabei!
         </h3>
-        <p class="opacity-40 dynamicLineHeight2 textJustify">
+        <p class="dynamicLineHeight2 textJustify dynamicTextColor">
           PURE.HONEST.MUSIC ist nicht nur eine Plattform für alternative Musikliebhaber*innen, sondern auch ein intuitiver Konzertkalender für den Raum Köln. Wir bieten allen, die auf der Suche nach spannenden und außergewöhnlichen Musikveranstaltungen sind, eine einfache Möglichkeit, Konzerte zu entdecken.<br><br> Unsere Mission ist es, die lebendige Musikszene Kölns noch sichtbarer zu machen und Menschen mit einer Leidenschaft für alternative Klänge zusammenzubringen. Auf unserer Plattform findest du eine handverlesene Auswahl an Konzerten und Events aus Genres wie Alternative, Indie, Post-Punk, Hip-Hop, Experimental und vielem mehr. Von etablierten Künstler*innen bis hin zu Newcomern – wir präsentieren dir die spannendsten musikalischen Highlights, die Köln zu bieten hat.<br><br> Unser Fokus liegt dabei auf der engen Zusammenarbeit mit lokalen Veranstalterinnen und Musikschaffenden, um die Vielfalt der Kölner Musikszene zu fördern. Besonders am Herzen liegt uns die Unterstützung von Kölner Nachwuchskünstlerinnen. Mit PURE.HONEST.MUSIC bieten wir ihnen eine Bühne und schaffen Gelegenheiten, ihre Musik einem breiteren Publikum zu präsentieren. Gleichzeitig laden wir dich ein, neue Klänge und spannende Talente zu entdecken, die vielleicht schon bald die großen Bühnen erobern. <br><br> Unsere Plattform inspiriert dich dazu, deinen musikalischen Horizont zu erweitern, neue Lieblingsgenres zu erkunden und die Vielfalt der Musikszene in Köln zu feiern. Egal, ob du nach einem intimen Indie-Konzert suchst oder die neuesten Experimente im Post-Punk erleben möchtest – bei PURE.HONEST.MUSIC wirst du fündig. Tauche ein in die Welt der Musik und entdecke das Beste, was die Kölner Musikszene zu bieten hat. <br> <br> Wir möchten darauf hinweisen, dass wir nicht als Veranstalter, Ticketverkäufer oder Ticket-Reseller fungieren. PURE.HONEST.MUSIC bietet eine Übersicht über aktuelle Konzerte im Raum Köln und leitet euch als Nutzer:innen direkt zu den entsprechenden Veranstaltern und Ticketanbietern weiter. PURE.HONEST.MUSIC stellt lediglich Informationen zu den Veranstaltungen bereit und übernimmt keine Verantwortung für den Ticketkauf oder die Abwicklung des Verkaufsprozesses.<br><br>PURE.HONEST.MUSIC ist ein unabhängiges Non-Profit-Projekt, das sich der Förderung authentischer Musik widmet. Wir verfolgen keinerlei kommerzielle Interessen und erzielen keine finanziellen Gewinne aus unseren Inhalten.
           Alle auf dieser Website veröffentlichten Beiträge, Interviews und Medien dienen ausschließlich der Unterstützung und Wertschätzung von Künstler*innen und ihrer Musik. Sollte dennoch ein Inhalt unbeabsichtigt gegen Rechte Dritter verstoßen, bitten wir um eine kurze Nachricht – wir werden das Anliegen umgehend prüfen und gegebenenfalls entsprechende Maßnahmen ergreifen.
           Vielen Dank für eure Unterstützung und euer Interesse an unserer Website! 
+          <br>
+          <br>
+          Du bist Musiker:in, organisierst Konzerte oder betreibst eine Location in Köln – und Deine Veranstaltung fehlt noch auf PURE.HONEST.MUSIC?
+          Dann schick uns eine kurze E-Mail mit den wichtigsten Angaben zu Deinem Event. Wir freuen uns über jede Nachricht und prüfen, ob Dein Konzert oder Festival in unsere kuratierte Auswahl passt.
+
+          Reiche dein Event <a href="mailto:info@purehonestmusic.com?subject=Konzertankündigung auf PURE.HONEST.MUSIC" class="text-primary underline">hier</a> ein!
         </p>
             <div class="mt-12 text-center">
-  <h4 class="text-text text-lg font-semibold mb-4">
+  <h4 class="uppercase text-text text-[18px] font-semibold mb-5">
     PURE.HONEST.MUSIC unterstützt die Kampagne <a href="https://www.keinkoelschfuernazis.cologne/die-kampagne" target="_blank" rel="noopener" class="text-primary underline hover:opacity-80">
       „KEIN KÖLSCH FÜR NAZIS„
     </a>
@@ -28,3 +34,9 @@
 </template>
 
 <script setup></script>
+
+<style>
+.dynamicTextColor {
+  color: #7c7c7c;
+}
+</style>
