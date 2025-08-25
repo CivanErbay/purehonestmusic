fa64ca52cb6e10564050b593db9b0cd89347a26a
Markus Gotzmann <markusgotzmann@Mac.fritz.box>
2025-08-25 16:05:29 +0200

    hotfix cookiebanner

--- components/CookieBanner.vue
+++ components/CookieBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="fixed bottom-8 w-4/5 lg:w-[800px] py-6 px-4 flex z-50 left-1/2 transform -translate-x-1/2 items-center bg-bg border border-primary rounded-xl">
-    <div class="">
+    class="fixed bottom-8 w-4/5 lg:w-[800px] py-6 px-4 flex z-[1000] left-1/2 -translate-x-1/2 items-center bg-bg border border-primary rounded-xl">
+    <div>
       <div class="flex flex-col flex-1">
         <p class="font-semibold">{{ settings.cookieHeadline }}</p>
         <p class="text-sm lg:text-base font-light py-2">
